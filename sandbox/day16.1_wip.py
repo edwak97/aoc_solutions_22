a494d5d3ef080554c2de099819d90bdaadcc1a7e
Eduard K <@edwak97>
2023-03-21 22:11:06 +0300

    Too slow!

--- sandbox/day16.1_wip.py
+++ sandbox/day16.1_wip.py
@@ -1,8 +1,12 @@
 import re
+import copy
 
+timeToRelease = 1
+beginValve = 'AA'
 T = 30
+
 lines = []
-with open('testinput.txt', 'r') as file_origin: 
+with open('input.txt', 'r') as file_origin: 
 		lines = file_origin.readlines()
 lines = [(lambda x:x)(line.strip()) for line in lines]
 
@@ -30,10 +34,54 @@ for key in valves: #getting the distance between this valve and other valves
 		front = newfront
 #building combinations
 #excluding valves producing nothing
-valves = {key:val for key,val in valves.items() if val['rate'] > 0}
-combination = [key for key in valves]
+valveNames = [key for key,val in valves.items() if val['rate'] > 0]
+print(valveNames)
+		
+def getCombinations(names):
+	if len(names) == 1:
+		yield names
+		return
 
-busy = [[i+x for x in range(i+2)] for i in range(len(combination)-1)]
-print(busy)
-	
-	
+	for i in range(len(names)):
+		prefix = [names[i]]
+		rest = copy.copy(names)
+		del(rest[i])
+		combination = getCombinations(rest)
+		while(True):
+			try:
+				result = prefix + next(combination)
+				yield result
+
+			except StopIteration:
+				break
+
+def getProductivity(names):
+	result = 0
+	_T = T
+	for k in range(len(names)-1):
+		distance = valves[names[k]]['valves'].get(names[k+1])
+		if distance == None:
+			result = None
+			print('This path is not possible')
+			break
+		_T -= (distance + timeToRelease)
+		if (T <= 0):
+			break
+		addendum = _T * valves[names[k+1]]['rate']
+		result += addendum
+	return result
+
+combination = getCombinations(valveNames)
+i = 1
+bestProductivity = 0
+while True:
+	try:
+		thisPath = [beginValve] + next(combination)
+		thisProductivity = getProductivity(thisPath)
+		if thisProductivity > bestProductivity:
+			bestProductivity = thisProductivity
+		i += 1	
+	except StopIteration:
+		break
+
+print(f'{i} combinations considered\n{bestProductivity}')
