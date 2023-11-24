b5c3b0db5d0f81878b5ffc79354dbeb8c74e0501
Eduard K <@edwak97>
2023-11-24 19:11:32 +0300

    Day 18 Part 1 [intentionally ugly]

--- day18.1.py
+++ day18.1.py
@@ -22,18 +22,8 @@ for i in range(3):
 	#when it is grouped we need to sort itc_dict[key] values by i coordinate
 
 	itc_dict = {(xyz_item.item[itc[0]], xyz_item.item[itc[1]]):sorted([_xyz_item.item[i] for _xyz_item in xyz_items if (_xyz_item.item[itc[0]],_xyz_item.item[itc[1]]) == (xyz_item.item[itc[0]], xyz_item.item[itc[1]])], key=lambda x:x) for xyz_item in xyz_items}
-	
-	itc_values = [itc_dict[key] for key in itc_dict]
-	del(itc_dict)
 
-	for itc_value in itc_values:
-		for c,value in enumerate(itc_value):
-			if c == 0:
-				continue
-			if(value - itc_value[c-1] == 1):
-				collisions += 1
+	collisions += sum([sum([(1 if x - itc_dict[key][c-1] == 1 else 0) for c,x in enumerate(itc_dict[key]) if c != 0]) for key in itc_dict])
 
 print(len(xyz_items)*6 -collisions*2)
-	
-	#the_res = [item[i] for val in itc_dict.items() for
-	
+
