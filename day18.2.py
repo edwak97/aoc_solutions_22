518fd25db1595abfec762cda35177f7431673d29
Eduard K <@edwak97>
2023-11-28 02:58:28 +0300

    Day 18 Part 2

--- day18.2.py
+++ day18.2.py
@@ -15,15 +15,13 @@ for line in lines}
 
 # Defining the "box" where the xyz-objects reside
 xyzLimits = { 
-	 'front': max(xyz_records.keys(), key=lambda val:val[0])[0]
-	 ,'back': min(xyz_records.keys(), key=lambda val:val[0])[0]
-	,'right': max(xyz_records.keys(), key=lambda val:val[1])[1]
-	 ,'left': min(xyz_records.keys(), key=lambda val:val[1])[1]
-	   ,'up': max(xyz_records.keys(), key=lambda val:val[2])[2]
-	 ,'down': min(xyz_records.keys(), key=lambda val:val[2])[2]
+	 'front': max(xyz_records.keys(), key=lambda val:val[0])[0]+1
+	 ,'back': min(xyz_records.keys(), key=lambda val:val[0])[0]-1
+	,'right': max(xyz_records.keys(), key=lambda val:val[1])[1]+1
+	 ,'left': min(xyz_records.keys(), key=lambda val:val[1])[1]-1
+	   ,'up': max(xyz_records.keys(), key=lambda val:val[2])[2]+1
+	 ,'down': min(xyz_records.keys(), key=lambda val:val[2])[2]-1
 }
-
-print(xyzLimits)
 
 #collision handler
 collisionPosition = {
@@ -35,20 +33,46 @@ collisionPosition = {
 	,'down':  lambda x,y,z: (x,y,z-1)
 }
 
-pairs = {
-	 'right': 'left'
-	,'front': 'back'
-	 ,'back': 'front'
-	 ,'left': 'right'
-	 ,'down': 'up'
-	   ,'up': 'down'
-}
-
 def checkXyz(xyz_tuple, side:str):
 	if side in {'front','back'}:
-		return xyzLimits['back']-1 <= xyz_tuple[0] <= xyzLimits['front']+1
+		return xyzLimits['back'] <= xyz_tuple[0] <= xyzLimits['front']
 	if side in {'right','left'}:
-		return xyzLimits['left']-1 <= xyz_tuple[1] <= xyzLimits['right']+1
+		return xyzLimits['left'] <= xyz_tuple[1] <= xyzLimits['right']
 	if side in {'up', 'down'}:
-		return xyzLimits['down']-1 <= xyz_tuple[2] <= xyzLimits['up']+1
+		return xyzLimits['down'] <= xyz_tuple[2] <= xyzLimits['up']
 
+#the structure is the storage of "dots" corresponding to empty space passed
+#the idea of this algorithm is to move from outside to inside and to "scan" all the sides available
+#begin from any edge of the container
+
+space_front = {(xyzLimits['front'], xyzLimits['right'], xyzLimits['up'])}
+space_passed = set()
+
+res = 0
+
+while(space_front):
+	
+	new_space_front = set()
+	for space_bit in space_front:
+		x,y,z = space_bit
+
+		for direction in directions:
+			#applying "move" toward direction
+			moveResult = collisionPosition[direction](x,y,z)
+			
+			#if the pointer is beyond the limits or the pointer is part of the front or was passed iteration ago then skip
+			if not checkXyz(moveResult, direction) or moveResult in space_passed or moveResult in space_front:
+				continue
+
+			if moveResult in xyz_records:
+				#the side of the cube marked counted
+				res += 1
+				xyz_records[moveResult][direction] = True
+				continue
+			
+			new_space_front.add(moveResult)
+	
+	space_passed = set(space_front)
+	space_front = set(new_space_front)
+
+print(res)
