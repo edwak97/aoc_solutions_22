49d1cb4a97b0abef3065352a6803b68e7f0f51a9
Eduard K <@edwak97>
2023-12-09 20:53:52 +0300

    Both parts + optimization (10.7s)

--- day19.py
+++ day19.py
@@ -1,7 +1,9 @@
 import re
 import sys
 import copy
+import time
 
+start_time = time.time()
 tmax = 32
 
 #order makes sense
@@ -19,7 +21,7 @@ with open('input.txt', 'r') as file_origin:
 
 #the dictionary 'a':{'b':val1, ... 'c':val2} means that there is a_bot type which requires b and c resource in the amount v1 and v2 respectively to produce a unit of a_bot
 
-lines = ['1 4 2 3 14 2 7', '2 2 3 3 8 3 12']
+#lines = ['1 4 2 3 14 2 7', '2 2 3 3 8 3 12']
 
 blueprints = [(lambda x:{'ore':{'ore':int(x[1])}, 'clay':{'ore':int(x[2])}, 'obsidian':{'ore':int(x[3]), 'clay':int(x[4])}, 'geode':{'ore':int(x[5]), 'obsidian': int(x[6])}})(re.findall(r'\d+', line)) for line in lines]
 
@@ -48,13 +50,20 @@ def doesMakeSense(mRes, bRes, bot, blueprint):
 	#consier we have 7 obsidian bots. Would buying yet another one benefit us?
 	#7 obsidian bots produce 7 unit that are enough to build a geode. Having 8 bots is excessive
 
-def combinationGenerator(mRes, bRes, T, blueprint, previous_availableToBuy, previous_option):
+def combinationGenerator(mRes, bRes, T, blueprint, previous_availableToBuy, previous_option, record_highs):
 	
 	#the bot has started the production; the first material unit will have been produced by the end of this minute
 	if T == 0:
 		yield mRes['geode']
 		return
 	
+	#some performance optimization using caching
+	bRes_record, mRes_record = record_highs[T-1]
+	if bRes['geode'] < bRes_record and mRes['geode'] < mRes_record:
+		return
+	if bRes['geode'] > bRes_record and mRes['geode'] > mRes_record:
+		record_highs[T-1] = (bRes['geode'], mRes['geode'])
+
 	availableToBuy = getAvailableToBuy(mRes)
 	for bot in availableToBuy:
 		#It does not make sense to buy a bot now if it was available to buy a minute before but skip was chosen!
@@ -71,7 +80,7 @@ def combinationGenerator(mRes, bRes, T, blueprint, previous_availableToBuy, prev
 		#new bot is ready!:
 		bRes_snapshot[bot] += 1
 			
-		combinations = combinationGenerator(mRes_snapshot, bRes_snapshot, T-1, blueprint, availableToBuy, bot)
+		combinations = combinationGenerator(mRes_snapshot, bRes_snapshot, T-1, blueprint, availableToBuy, bot, record_highs)
 
 		while(True):
 			try:
@@ -86,7 +95,7 @@ def combinationGenerator(mRes, bRes, T, blueprint, previous_availableToBuy, prev
 	#skipping: no new bots, but those made before has produced something;
 	mRes_snapshot = {material:(mRes[material]+bRes[material]*productivity) for material in mRes}
 	
-	combinations = combinationGenerator(mRes_snapshot, copy.deepcopy(bRes), T-1, blueprint, availableToBuy, None)
+	combinations = combinationGenerator(mRes_snapshot, copy.deepcopy(bRes), T-1, blueprint, availableToBuy, None, record_highs)
 	while(True):
 		try:
 			yield(next(combinations))
@@ -96,9 +105,9 @@ def combinationGenerator(mRes, bRes, T, blueprint, previous_availableToBuy, prev
 #part1
 tmax = 24
 res = 0
-for i,blueprint in enumerate(blueprints):
-	
-	test_result = combinationGenerator(m_resource, b_resource, tmax, blueprint, [], None)
+for i,blueprint in enumerate(blueprints):	
+	record_highs = [(0,0) for i in range(tmax)]
+	test_result = combinationGenerator(m_resource, b_resource, tmax, blueprint, [], None, record_highs)
 	best_result = 0
 	while(True):
 		try:
@@ -112,8 +121,8 @@ for i,blueprint in enumerate(blueprints):
 res = 1
 blueprints = blueprints[:3]
 for blueprint in (blueprints):
-	
-	test_result = combinationGenerator(m_resource, b_resource, tmax, blueprint, [], None)
+	record_highs = [(0,0) for i in range(tmax)]
+	test_result = combinationGenerator(m_resource, b_resource, tmax, blueprint, [], None, record_highs)
 	best_result = 0
 	while(True):
 		try:
@@ -122,8 +131,7 @@ for blueprint in (blueprints):
 				best_result = thisPath
 		except StopIteration:
 			break
-	print(best_result)
 	res *= best_result
 
 print(res)
-
+print(f'Execution required: {time.time() - start_time}')
