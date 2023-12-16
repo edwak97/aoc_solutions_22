674108fbb29cefe10fe5d3f347f45460fc39012e
Eduard K <@edwak97>
2023-12-16 03:27:29 +0300

    bugfix

--- sandbox/day20.1.py
+++ sandbox/day20.1.py
@@ -15,14 +15,20 @@ class NumData:
 		self.numbers = numbers
 		self.indexes = {i:i for i in range(len(numbers))}
 	
-	def obtainShift(self, shift:int)->int:
+	def obtainShift(self, index:int, shift:int)->int:		
+		if ((index == 0) and shift > 0) or ((index == len(self.numbers)-1) and shift < 0):
+			if abs(shift) >= len(self.numbers):
+				new_index = (index + 1) if shift > 0 else (index - 1)
+				new_shift = (1 if shift > 0 else -1) * (abs(shift)-len(self.numbers))
+				return (1 if shift > 0 else -1)+self.obtainShift(new_index, new_shift)
+			return shift
 		return (1 if shift > 0 else -1) * (abs(shift) % (len(self.numbers)-1))
 
 	#shifts element of numbers[index] to the index+shift position
 	#both index and index+shift must be in [0;len(numbers)-1]
 	def shiftData(self, index:int, shift:int):
 		
-		#shift = self.obtainShift(shift)
+		shift = self.obtainShift(index, shift)
 		
 		new_index = index + shift
 		
@@ -45,6 +51,7 @@ class NumData:
 
 #tests:
 #general shift
+
 nums1 = NumData([5,4,3,2,1,0])
 nums1.shiftData(0,5)
 assert nums1.numbers == [4,3,2,1,0,5]
@@ -72,12 +79,34 @@ nums1.shiftData(0,6)
 assert nums1.numbers == [9,5,6,7,8,-2,4]
 nums1.shiftData(3,6)
 assert nums1.numbers == [9,5,6,7,8,-2,4]
+nums1.shiftData(0,6)
+assert nums1.numbers == [5,6,7,8,-2,4,9]
 
 nums1.numbers = [0,1,2,3,4,5,6,7,8,9]
-assert nums1.obtainShift(1) == 1
-assert nums1.obtainShift(-1) == -1
-assert nums1.obtainShift(10) == 1
-assert nums1.obtainShift(9) == 0
-assert nums1.obtainShift(-110) == -2
-assert nums1.obtainShift(0) == 0
-assert nums1.obtainShift(25) == 7
+nums1.shiftData(1,9)
+assert nums1.numbers == [0,1,2,3,4,5,6,7,8,9]
+nums1.shiftData(1,18)
+assert nums1.numbers == [0,1,2,3,4,5,6,7,8,9]
+nums1.shiftData(9,18)
+assert nums1.numbers == [0,1,2,3,4,5,6,7,8,9]
+nums1.shiftData(9,-19)
+assert nums1.numbers == [0,1,2,3,4,5,6,7,9,8]
+
+nums1.numbers = [0,1,2,3,4,5,6,7,8,9]
+nums1.shiftData(0,-9)
+assert nums1.numbers == [0,1,2,3,4,5,6,7,8,9]
+nums1.shiftData(0,51)
+assert nums1.numbers == [1,2,3,4,5,6,0,7,8,9]
+nums1.shiftData(9,-51)
+assert nums1.numbers == [1,2,3,9,4,5,6,0,7,8]
+nums1.shiftData(0,4)
+assert nums1.numbers == [2,3,9,4,1,5,6,0,7,8]
+nums1.shiftData(9,-1)
+assert nums1.numbers == [2,3,9,4,1,5,6,0,8,7]
+nums1.shiftData(4,111)
+assert nums1.numbers == [2,3,9,4,5,6,0,1,8,7]
+nums1.shiftData(7,111)
+assert nums1.numbers == [2,1,3,9,4,5,6,0,8,7]
+nums1.shiftData(1,-578)
+assert nums1.numbers == [2,3,9,4,5,6,0,8,1,7]
+
