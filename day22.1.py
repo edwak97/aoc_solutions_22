13889351d50efc503c963861b5974a5292f5b4a0
Eduard K <@edwak97>
2024-01-06 21:25:37 +0300

    Day 22 Part 1 optimization

--- day22.1.py
+++ day22.1.py
@@ -12,14 +12,17 @@ steps = lines.pop()
 
 steps = re.findall(r'L|R|\d+', steps)
 
-def obtainDescription(y_val:int, x_val:int, positions):
-	
+def obtainDescription(y_val:int, x_val:int, positions, max_val_y, max_val_x):
+	'''
 	x_vals = [x for (y,x) in positions if y == y_val]
 	y_vals = [y for (y,x) in positions if x == x_val]
-
+	
 	min_x,max_x = min(x_vals),max(x_vals)
 	min_y,max_y = min(y_vals),max(y_vals)
-	
+	'''
+	min_x,max_x = max_val_x[y_val]
+	min_y,max_y = max_val_y[x_val]
+
 	result = dict()
 	
 	if y_val+1 > max_y and positions[(coords:=(min_y,x_val))]:
@@ -67,7 +70,11 @@ def tryMove(direction, pos, step, positions):
 
 positions = {(line_number,char_position):char!='#' for line_number,line in enumerate(lines) for char_position,char in enumerate(line) if char != ' '}
 
-positions = {(y,x):obtainDescription(y,x,positions) for (y,x) in positions if positions[(y,x)]}
+edge_values_x = {y_val:(lambda lst: (min(lst),max(lst)))({x for (y,x) in positions if y==y_val}) for y_val in {y for (y,_) in positions}}
+edge_values_y = {x_val:(lambda lst: (min(lst),max(lst)))({y for (y,x) in positions if x==x_val}) for x_val in {x for (_,x) in positions}}
+
+positions = {(y,x):obtainDescription(y,x,positions,edge_values_y,edge_values_x) for (y,x) in positions if positions[(y,x)]}
+
 
 init_direction = 'right'
 init_position = (0,min([x for (y,x) in positions if y==0]))
